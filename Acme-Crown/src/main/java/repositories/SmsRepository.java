c345235b7fe573ea5fb2cae33fa56334b506ae70
jospalhid <Es25Alba>
2017-05-24 19:58:16 +0200

    Mensajes

--- Acme-Crown/src/main/java/repositories/SmsRepository.java
+++ Acme-Crown/src/main/java/repositories/SmsRepository.java
@@ -12,10 +12,10 @@ import domain.Sms;
 @Repository
 public interface SmsRepository extends JpaRepository<Sms, Integer> {
 
-	@Query("select s from Sms s where s.recipient.userAccount.id=?1")
+	@Query("select s from Sms s where s.recipient.userAccount.id=?1 order by s.moment desc")
 	Collection<Sms> findMyReceivedMessages(int uaId);
 
-	@Query("select s from Sms s where s.sender.userAccount.id=?1")
+	@Query("select s from Sms s where s.sender.userAccount.id=?1 order by s.moment desc")
 	Collection<Sms> findMySendMessages(int uaId);
 	
 	@Query("select count(s) from Sms s where s.recipient.userAccount.id=?1 and s.readed=false")
